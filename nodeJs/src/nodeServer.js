378193b156ab75c91efbfa41968ebb303b30091e
ztree <hunter.z@263.net>
2017-02-07 16:43:40 +0800

    no message

--- nodeJs/src/nodeServer.js
+++ nodeJs/src/nodeServer.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/../../page_src'));
+console.log('Web Path: ' + __dirname + '/../../page_src');
 
 app.listen(port, hostname, function () {
     console.log(`Server running at http://${hostname}:${port}/`);
